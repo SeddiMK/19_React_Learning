01699444185a166f3d805f2d049e565b16a24e08
SeddiMK <web.egorovm@gmail.com>
2023-06-22 13:18:32 +0700

    Unit_01

--- unit_1/src/App.js
+++ unit_1/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+let text = 'test variable';
+
 function App() {
+  const style = {
+    fontSize: '24px',
+    fontStyle: 'italic',
+    color: 'red',
+  };
+  let text2 = '222';
   return (
     <>
       <div className="container">
-        <h1>app_1</h1>
+        <h1 style={style}>app_1</h1>
+        <img src="/images/test.webp" alt="vegetables" />
+        <ul>
+          <li>Hello</li>
+          <li>{text + text2}</li>
+        </ul>
       </div>
     </>
   );
