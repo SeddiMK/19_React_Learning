16df190abf80d8e179911c37c3b883cfc62c006d
SeddiMK <web.egorovm@gmail.com>
2023-06-28 13:45:18 +0700

    unit_04_onClick-function-metods-input

--- unit_04/src/App.js
+++ unit_04/src/App.js
@@ -2,69 +2,67 @@ import './App.css';
 import React from 'react';
 import { useState } from 'react';
 
-
 function App() {
   let count4 = 0;
 
   function task1() {
-
+    console.log('task2');
   }
-  function task2() {
-
+  function task2(event) {
+    event.target.classList.add('active');
   }
-  function task3() {
-
+  function task3(event) {
+    console.log(event.target.value);
   }
   function task4() {
-
+    count4++;
+    console.log(count4);
   }
-  function task5() {
-
+  function task5(event) {
+    if (event.target.checked) {
+      document.querySelector('.out-5').textContent = event.target.value;
+    } else {
+      document.querySelector('.out-5').textContent = '0';
+    }
   }
-  function task6() {
-
-  }
-  function task7() {
-
-  }
-  function task8() {
-
-  }
-  function task9() {
-
-  }
+  function task6() {}
+  function task7() {}
+  function task8() {}
+  function task9() {}
   let ar10 = [5, 6, 7];
-  function task10() {
-
-  }
+  function task10() {}
 
   return (
     <>
       <h1>События</h1>
       <section>
         <h2>Task 1</h2>
-        <button className="task-1">Push</button>
+        <button className="task-1" onClick={task1}>
+          Push
+        </button>
       </section>
       <section>
         <h2>Task 2</h2>
-        <div className="task-2"></div>
+        <div className="task-2" onMouseEnter={task2}></div>
       </section>
       <section>
         <h2>Task 3</h2>
-        <input type="text" className="task-3" />
+        <input type="text" className="task-3" onInput={task3} />
       </section>
       <section>
         <h2>Task 4</h2>
-        <button className="task-4">Count</button>
+        <button className="task-4" onClick={task4}>
+          Count
+        </button>
       </section>
       <section>
         <h2>Task 5</h2>
-        <input type="checkbox" currentValue="55" />
+        <input type="checkbox" currentValue="55" onChange={task5} />
         <div className="out-5"></div>
       </section>
       <section>
         <h2>Task 6</h2>
-        <select className="task-6">
+        <select className="task-6" onChange={task6}>
           <option value="7">seven</option>
           <option value="4">four</option>
           <option value="9">nine</option>
