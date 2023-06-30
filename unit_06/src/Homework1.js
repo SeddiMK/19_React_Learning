504be15119b293cddfe44d24c6eb960359c43185
SeddiMK <web.egorovm@gmail.com>
2023-06-30 15:01:23 +0700

    unit_06-liveCicle

--- unit_06/src/Homework1.js
+++ unit_06/src/Homework1.js
@@ -5,14 +5,33 @@ class Homework1 extends React.Component {
     console.log(props);
     super();
     this.state = {
-      s1: props,
+      s1: props.p1,
     };
     this.s2 = 201;
   }
 
+  DidMount = () => {
+    let valS1 = this.state.s1;
+    let valS2 = this.s2;
+    valS1 = valS1 + 5;
+    valS2 = valS2 + 5;
+    this.setState({ s1: valS1 });
+    this.s2 = valS2;
+  };
+
+  buttonHander = () => {
+    let valS1 = this.state.s1;
+    let valS2 = this.s2;
+    valS1 = valS1 + 50;
+    valS2 = valS2 + 50;
+    this.setState({ s1: valS1 });
+    this.s2 = valS2;
+  };
+
   render() {
     return (
       <>
+        <button onClick={this.buttonHander}>Push</button>
         <div>{this.state.s1}</div>
         <div>{this.s2}</div>
       </>
