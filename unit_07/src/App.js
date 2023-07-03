0ba967ad10be678d67adc05aa3df32fadbc7a3b5
SeddiMK <web.egorovm@gmail.com>
2023-07-03 13:28:40 +0700

    unit_07-ref-dom(Cart_in_Shops)

--- unit_07/src/App.js
+++ unit_07/src/App.js
@@ -5,7 +5,24 @@ import Goods from './Goods';
 import goodsArr from './goods.json';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <h1>Cart</h1>
+        <div className="goods-field">
+          {goodsArr.map((item) => (
+            <Goods
+              title={item.title}
+              cost={item.cost}
+              image={item.image}
+              articul={item.articul}
+              key={item.articul}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
